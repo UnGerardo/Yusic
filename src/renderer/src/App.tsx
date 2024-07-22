b0eaf9fb4602d5d6166592923af71b370e8e4ae4
UnGerardo <gerardojdls@gmail.com>
2024-07-21 21:59:58 -0700

    Set up basic page layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,33 +1,31 @@
 import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
+      <section id="options">
+        <span>Select Music Folder</span>
+      </section>
+      <main>
+        <section id="groups">
+          <span>Tracks</span>
+          <span>Artists</span>
+          <span>Albums</span>
+        </section>
+        <section id="items">
+          <img src="" alt="" />
+          <span>Track Name</span>
+          <span>Artist</span>
+          <span>Album</span>
+          <span>Time</span>
+        </section>
+      </main>
+      {/* <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+        Send IPC
+      </a>
+      <Versions></Versions> */}
     </>
   )
 }
