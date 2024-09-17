f0b24f5b71a57b0849953e95b110b0928771d336
UnGerardo <gerardojdls@gmail.com>
2024-09-17 13:00:00 -0700

    - Modified Track class props

--- src/classes/Track.ts
+++ src/classes/Track.ts
@@ -3,20 +3,20 @@ import { IAudioMetadata } from "music-metadata";
 export default class Track {
   id: number | undefined;
   path: string;
-  title: string | undefined;
-  artists: string | undefined;
-  album: string | undefined;
-  duration: number | undefined;
-  imgFormat: string | undefined;
+  title: string;
+  artists: string;
+  album: string;
+  duration: number;
+  imgFormat: string;
   imgData: string;
 
   constructor(metadata: IAudioMetadata = {} as IAudioMetadata, path: string = '', pictureData: string = '') {
     this.path = path;
-    this.title = metadata.common.title;
-    this.artists = metadata.common.artist;
-    this.album = metadata.common.album;
-    this.duration = metadata.format.duration;
-    this.imgFormat = metadata.common.picture?.at(0)?.format;
+    this.title = metadata.common.title ?? 'None';
+    this.artists = metadata.common.artist ?? 'None';
+    this.album = metadata.common.album ?? 'None';
+    this.duration = metadata.format.duration ?? 0;
+    this.imgFormat = metadata.common.picture?.at(0)?.format ?? 'None';
     this.imgData = pictureData;
   }
 }
