21a7ef95e8de0d541fa32009903ec3ea294569f0
UnGerardo <gerardojdls@gmail.com>
2024-09-21 15:00:00 -0700

    - Created Settings and ClearQueue icons

--- src/renderer/src/components/ActionBar.tsx
+++ src/renderer/src/components/ActionBar.tsx
@@ -2,12 +2,16 @@ import styled from "styled-components";
 import ReadMusicFolder from "./ReadMusicFolder";
 import SearchQuery from "./SearchQuery";
 import Shuffle from "./Shuffle";
+import { Settings } from "./Settings";
+import { ClearQueue } from "./ClearQueue";
 
 export const ActionBar = (): JSX.Element => (
   <Bar>
+    <Settings />
     <ReadMusicFolder />
     <SearchQuery />
     <Shuffle />
+    <ClearQueue />
   </Bar>
 );
 
