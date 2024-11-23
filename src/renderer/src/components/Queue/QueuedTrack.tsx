03c0091c0b91a90fec5d0bf1a52fa7de19100f45
UnGerardo <gerardojdls@gmail.com>
2024-11-22 22:37:22 -0800

    - Added <RemoveButton> to remove a track from the queue

--- src/renderer/src/components/Queue/QueuedTrack.tsx
+++ src/renderer/src/components/Queue/QueuedTrack.tsx
@@ -7,6 +7,7 @@ import { TrackArtist, TrackImage, TrackInfo, TrackTitle } from "@renderer/assets
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import ReactTrack from "@renderer/react-classes/ReactTrack";
+import updateTrackIndicies from "@renderer/utils/updateTrackIndicies";
 
 const QueuedTrack = ({ index, track, style, activeTrack }:
   {
@@ -15,7 +16,7 @@ const QueuedTrack = ({ index, track, style, activeTrack }:
     style: React.CSSProperties,
     activeTrack: ReactTrack | null
   }): JSX.Element => {
-  const { queueIndex, setQueueIndex } = useContext(QueueContext);
+  const { setQueue, queueIndex, setQueueIndex } = useContext(QueueContext);
   const { setPlayingTrack } = useContext(PlayingTrackContext)
   const { setAudioSource } = useContext(AudioSourceContext);
 
@@ -40,6 +41,30 @@ const QueuedTrack = ({ index, track, style, activeTrack }:
     setAudioSource(track.path);
   }
 
+  const removeFromQueue = (e) => {
+    e.stopPropagation();
+    setQueue((queue) => {
+      queue.splice(index, 1);
+      if (index === queueIndex) {
+        // if removed track was the last
+        if (index === queue.length) {
+          const previousIndex = queueIndex - 1;
+          setQueueIndex(previousIndex)
+          setPlayingTrack(queue[previousIndex]);
+          setAudioSource(queue[previousIndex].path);
+        } else {
+          setPlayingTrack(queue[queueIndex]);
+          setAudioSource(queue[queueIndex].path);
+        }
+      } else if (index < queueIndex) {
+        setQueueIndex(queueIndex - 1);
+      }
+      // Use updateTrackIndicies() to prevent unnecessary re-creation of tracks
+      updateTrackIndicies(queue);
+      return [...queue];
+    });
+  }
+
   return (
     <StyledQueuedTrack
       isCurrentTrack={index === queueIndex}
@@ -55,16 +80,53 @@ const QueuedTrack = ({ index, track, style, activeTrack }:
         <TrackTitle>{track.title}</TrackTitle>
         <TrackArtist>{track.artists}</TrackArtist>
       </TrackInfo>
+      <RemoveButton onClick={removeFromQueue} />
     </StyledQueuedTrack>
   );
 };
 
 export default QueuedTrack;
 
+const RemoveButton = styled.button`
+  background: none;
+  border: none;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  height: 18px;
+  width: 18px;
+  position: relative;
+  transform: rotate(45deg);
+
+  &:before {
+    background-color: gray;
+    content: '';
+    height: 2px;
+    width: 18px;
+    position: absolute;
+  }
+
+  &:after {
+    background-color: gray;
+    content: '';
+    height: 18px;
+    width: 2px;
+    position: absolute;
+  }
+
+  &:hover { border-color: white; }
+  &:hover::after { background-color: white; }
+  &:hover::before { background-color: white; }
+
+  &:active { border-color: #666; }
+  &:active::after { background-color: #666; }
+  &:active::before { background-color: #666; }
+`;
+
 const StyledQueuedTrack = styled.section<{ isCurrentTrack: boolean }>`
   background-color: ${(props) => props.isCurrentTrack ? 'rgba(61, 61, 61, 0.5)' : 'none'};
   display: grid;
-  grid-template-columns: 18px 50px 1fr ;
+  grid-template-columns: 18px 50px 1fr 18px;
   align-items: center;
   gap: 10px;
   padding: 5px 5px 5px 10px;
@@ -73,6 +135,10 @@ const StyledQueuedTrack = styled.section<{ isCurrentTrack: boolean }>`
   &:hover {
     background-color: rgba(61, 61, 61, 0.5);
     cursor: pointer;
+
+    ${RemoveButton} {
+      display: flex;
+    }
   }
 `;
 
